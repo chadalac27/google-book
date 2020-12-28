06afeb4eff32e8552f73b8706b723e7ed8fb3211
chadalac27 <chadcoleman1234@gmail.com>
2020-12-28 00:26:29 -0800

    fixed problem with mono database

--- models/books.js
+++ models/books.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const booksSchema = new Schema({
   title: {type: String},
   author: {type: String},
   description: {type: String},
   image: {type:String},
   url: {type:String}
+  
 });
 
 const Books = mongoose.model("Books", booksSchema);
+
 module.exports = Books;
