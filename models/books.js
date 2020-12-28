d9fda751988ed48af7daf8a918c073db9755d6f2
chadalac27 <chadcoleman1234@gmail.com>
2020-12-27 23:39:41 -0800

    Added controllers,routes,api,and models.  Alos found a bug with import instead of const require

---
+++ models/books.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const booksSchema = new Schema({
+  title: {type: String},
+  author: {type: String},
+  description: {type: String},
+  image: {type:String},
+  url: {type:String}
+});
+
+const Books = mongoose.model("Books", booksSchema);
+module.exports = Books;
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-import express from "express";
-import mongoose from "mongoose";
-import path from "path";
-import morgan from "morgan";
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 require('dotenv').config();
 
-app.use(morgan("common"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/client/build"));
 }
+
+app.use(routes);
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
