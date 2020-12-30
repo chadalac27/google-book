71ad01b25addd5a6d933c583febc5999448bbc71
chadalac27 <chadcoleman1234@gmail.com>
2020-12-29 20:12:51 -0800

    added template for home and search

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+const Home = (props) => {
+  
+  
+  return (
+    <div>
+      <Grid xs={12} container direction="column">
+
+      </Grid>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import NavBar from "../components/NavBar";
+
+const Search = (props) => {
+
+  return (
+    <div>
+      <Grid container direction="column">
+      </Grid>
+    </div>
+  );
+};
+
+export default Search;
