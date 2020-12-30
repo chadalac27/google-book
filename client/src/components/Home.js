714aeaf37c7dc136111dbb5a52a297353066600d
chadalac27 <chadcoleman1234@gmail.com>
2020-12-29 20:47:52 -0800

    Finished the base functionality of the book search and save

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid } from "@material-ui/core";
+import Navigation from "../components/Navigation";
+import BookList from "../components/BookList";
+import API from '../utils/API';
 
 const Home = (props) => {
   
-  
+  const [savedBooks, setSavedBooks] = useState([]);
+
+    useEffect(() => {
+        API.getBooks().then((res) =>{
+            console.log(res.data);
+            setSavedBooks(res.data);
+        });
+    },[]);
+
   return (
     <div>
+      <Navigation />
       <Grid xs={12} container direction="column">
-
+        <BookList actionName="Delete" books={savedBooks} setBooks={setSavedBooks} title="Saved Books" />
       </Grid>
     </div>
   );
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
-import NavBar from "../components/NavBar";
+import Navigation from "../components/Navigation";
+import BookList from "../components/BookList";
+import SearchField from '../components/SearchField';
 
 const Search = (props) => {
+  const [searchBooks, setsearchBooks] = useState([]);
 
   return (
     <div>
+      <Navigation />
       <Grid container direction="column">
+        <SearchField books={searchBooks} setBooks={setsearchBooks}/>
+        <BookList actionName="Add" books={searchBooks} setBooks={setsearchBooks} title="Results" />
       </Grid>
     </div>
   );
